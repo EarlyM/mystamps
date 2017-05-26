6c7c27caa57eb76eac32023a55e16c28716049c4
Slava Semushin <slava.semushin@gmail.com>
2017-05-26 16:53:55 +0200

    add controller

---
+++ src/main/java/ru/mystamps/web/controller/TestController.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2009-2017 Slava Semushin <slava.semushin@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package ru.mystamps.web.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ru.mystamps.web.Url;
+
+@Controller
+public class TestController {
+	
+	@GetMapping("/test/response/301")
+	public void redirect(HttpServletResponse response) {
+		response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+		response.setHeader("Location", Url.SITE);
+	}
+	
+	@GetMapping("/test/response/404")
+	public void notFound(HttpServletResponse response) throws IOException {
+		response.sendError(HttpServletResponse.SC_NOT_FOUND);
+	}
+	
+	@GetMapping("/test/empty/jpeg-file")
+	public void emptyJpegFile(HttpServletResponse response) {
+		response.setContentType("image/jpeg");
+		response.setContentLength(0);
+	}
+	
+}
