ad6ad1279141437b2b6bcdbe5444cf5b2adde575
Slava Semushin <slava.semushin@gmail.com>
2017-08-31 12:27:28 +0200

    fix jacoco

--- src/main/scripts/ci/publish-code-coverage.sh
+++ src/main/scripts/ci/publish-code-coverage.sh
@@ -14,13 +14,9 @@ set -o pipefail
 
 JACOCO_FAIL=
 
-mvn --batch-mode jacoco:prepare-agent test jacoco:report coveralls:jacoco -Denforcer.skip=true -DskipMinify=true >jacoco.log 2>&1 || JACOCO_FAIL=yes
+mvn --batch-mode jacoco:prepare-agent test jacoco:report coveralls:jacoco -Denforcer.skip=true -DskipMinify=true || JACOCO_FAIL=yes
 
 print_status "$JACOCO_FAIL" 'Publish code coverage'
-
-print_log jacoco.log 'Publish code coverage'
-
-rm -f jacoco.log
 
 if [ -n "$JACOCO_FAIL" ]; then
 	exit 1
